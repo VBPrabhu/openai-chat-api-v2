5f639c659602f7ac87b762571a13bc894a1000dc
Venkatesh Prabhu <VBPrabhu@users.noreply.github.com>
2025-03-20 07:52:49 +0100

    Update models.js

--- routes/models.js
+++ routes/models.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const openaiApi = require('../config/openai');
+const openaiApi = require('../openai');
 const router = express.Router();
 
 router.get('/v1/models', async (req, res) => {
